b033e4734355bbe2782fc95e0d918971473be5fa
aaron <aaron@bumblebeespaces.com>
2020-11-28 18:37:57 -0800

    feat (shader): add firepit colmap shader

--- pc3_gpu.py
+++ pc3_gpu.py
@@ -166,57 +166,96 @@ class PC3(Window):
     title = "PC3 Visualizer"
     gl_version = (3, 4)
 
+    def load_vertex_shader(self):
+        vertex_shader='''
+            #version 330
+
+            uniform mat4 Mvp;
+
+            in vec3 in_move;
+
+            in vec3 in_position;
+            in vec3 in_normal;
+
+            out vec3 v_vert;
+            out vec3 v_norm;
+
+            void main() {
+                float s = 1.0;
+                gl_Position = Mvp * vec4(s*in_position + in_move, 1.0);
+                v_vert = in_position + in_move;
+                v_norm = in_normal;
+            }
+            '''
+        return vertex_shader
+
+    def load_fragment_shader(self, name):
+
+        f_shader_firepit='''
+            #version 330
+
+            uniform vec3 Light;
+
+            in vec3 v_vert;
+            in vec3 v_norm;
+
+            out vec4 f_color;
+
+            void main() {
+                float dist = length((v_vert - 0))/255.0;
+                float lum = clamp(dot(normalize(Light - v_vert), normalize(v_norm)), 0.0, 1.0) * 0.3 + 0.7;
+
+                //float proximity = 1.0 - length(v_vert) / 255.0;
+                lum = 1.0;
+
+                // front red back blue
+                float b = clamp(dist * 1.3, 0.0, 1.0);
+                float r = 1.0 - b;
+                f_color = vec4(lum*r, 0.1, lum*b, 1.0);
+
+            }
+        '''
+
+        f_shader_modulated='''
+            #version 330
+
+            uniform vec3 Light;
+
+            in vec3 v_vert;
+            in vec3 v_norm;
+
+            out vec4 f_color;
+
+            void main() {
+                float lum = clamp(dot(normalize(Light - v_vert), normalize(v_norm)), 0.0, 1.0) * 0.7 + 0.3;
+                //f_color = vec4(v_vert.z / 255.0, 0.0, 1.0 - v_vert.z / 255.0, 1.0);
+
+                // center is red and lit
+                //float b = (abs(v_vert.z*2)/255.0);
+                //float r = 1.0 - b;
+
+                // front red back blue
+                float b = -(v_vert.z-128)/255.0;
+                float r = 1.0 - b;
+
+                f_color = vec4(lum*r, abs(sin(b*9.0)), 0.6*b, 1.0);
+
+                //f_color = vec4(v_vert.z / 255.0, v_vert.z  / 255.0, v_vert.z / 255.0, 1.0);
+            }
+        '''
+        fragment_shader = {
+            'modulated' : f_shader_modulated,
+            'firepit' : f_shader_firepit
+        }
+        return fragment_shader[name]
+
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         self.prog = self.ctx.program(
-            vertex_shader='''
-                #version 330
-
-                uniform mat4 Mvp;
-
-                in vec3 in_move;
-
-                in vec3 in_position;
-                in vec3 in_normal;
- 
-                out vec3 v_vert;
-                out vec3 v_norm;
-
-                void main() {
-                    float s = 1.1;
-                    gl_Position = Mvp * vec4(s*in_position + in_move, 1.0);
-                    v_vert = s*in_position + in_move;
-                    v_norm = in_normal;
-                }
-            ''',
-            fragment_shader='''
-                #version 330
-
-                uniform vec3 Light;
-
-                in vec3 v_vert;
-                in vec3 v_norm;
-
-                out vec4 f_color;
-
-                void main() {
-                    float lum = clamp(dot(normalize(Light - v_vert), normalize(v_norm)), 0.0, 1.0) * 0.7 + 0.3;
-                    //f_color = vec4(v_vert.z / 255.0, 0.0, 1.0 - v_vert.z / 255.0, 1.0);
-
-                    // center is red and lit
-                    //float b = (abs(v_vert.z*2)/255.0);
-                    //float r = 1.0 - b;
-
-                    // front red back blue
-                    float b = -(v_vert.z-128)/255.0;
-                    float r = 1.0 - b;
-
-                    f_color = vec4(lum*r, abs(sin(b*9.0)), 0.6*b, 1.0);
-
-                    //f_color = vec4(v_vert.z / 255.0, v_vert.z  / 255.0, v_vert.z / 255.0, 1.0);
-                }
-            ''',
+            vertex_shader=self.load_vertex_shader(),
+            fragment_shader=self.load_fragment_shader("firepit"),
         )
 
         self.off = {'x' : 0, 'y' : 0 ,'z' : 0}
