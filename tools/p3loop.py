09e762334dca65beae91e1ae593d9c9b0f11e3e4
aaron <aaron@bumblebeespaces.com>
2020-12-27 23:35:36 -0800

    tools: add augmented guidance

--- tools/p3loop.py
+++ tools/p3loop.py
@@ -9,6 +9,19 @@ def loop(data, file):
         i = cv2.imread(file);
         h, w = i.shape[0:2]
         i = cv2.resize(i, (w*s, h*s), interpolation=cv2.INTER_NEAREST)
+
+        #1
+        p0, p1 = (0*s, 75*s), ((w-1)*s, 47*s)
+        cv2.line(i, p0, p1, (0,0,255))
+        p0, p1 = (0*s, 82*s), ((w-1)*s, 55*s)
+        cv2.line(i, p0, p1, (0,205,255))
+
+        #2
+        p0, p1 = (0*s, 67*s), ((w-1)*s, 86*s)
+        cv2.line(i, p0, p1, (0,205,255))
+        p0, p1 = (0*s, 63*s), ((w-1)*s, 82*s)
+        cv2.line(i, p0, p1, (0,0,255))
+ 
         cv2.imshow(file, i);
         key = cv2.waitKey(100)
 
